9303a94c61a3e3e82bf69a041d281fcbb713cf7f
Sebastian Scherbes <github@scherbes.de>
2022-09-27 15:37:45 +0200

    add create player function

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import styled from "styled-components";
 import ResetButton from "./components/ResetButton";
 import Player from "./components/Player";
 import PlayerForm from "./components/PlayerForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [players, setPlayers] = useState([]);
+  const [newPlayer, setNewPlayer] = useState("");
+
+  useEffect(() => {
+    console.log(players);
+  }, [players]);
+
+  const onCreateNewPlayer = (event) => {
+    event.preventDefault();
+    setPlayers((players) => [
+      ...players,
+      {
+        id: uuid().slice(0, 8),
+        name: newPlayer,
+        score: 0,
+      },
+    ]);
+    setNewPlayer("");
+  };
 
   return (
     <Main>
       <Header>Scorekeeper Version A</Header>
-      <PlayerForm>
+      <PlayerForm onSubmit={onCreateNewPlayer}>
         <PlayerList>
-          <Player name="John Doe" score={9} />
+          {players.map((player) => (
+            <Player key={player.id} name={player.name} score={player.score} />
+          ))}
         </PlayerList>
         <ResetButton>Reset scores</ResetButton>
         <ResetButton>Reset all</ResetButton>
         <Header2>Add Player:</Header2>
-        <NewPlayer />
+        <NewPlayer
+          onChange={(event) => setNewPlayer(event.target.value)}
+          value={newPlayer}
+        />
       </PlayerForm>
     </Main>
   );
@@ -42,6 +66,7 @@ const Header2 = styled.h2`
 const PlayerList = styled.ul`
   width: 80%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
 `;
 
--- src/components/NewPlayer/index.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const NewPlayer = styled.input``;
-
-export default NewPlayer;

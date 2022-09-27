d0e16c3b28252cd280344e04f8d2a5a25159c62a
Sebastian Scherbes <github@scherbes.de>
2022-09-27 11:49:03 +0200

    add styled-component Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  return <Main></Main>;
 }
 
 export default App;
+
+const Main = styled.main`
+  text-align: center;
+`;

db338a5dad8c38bf678a1e56ea2c19a00aea4a92
Sebastian Scherbes <github@scherbes.de>
2022-09-27 12:11:47 +0200

    add global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import ScoreBox from "./components/ScoreBox";
 import Button from "./components/Button";
+import Player from "./components/Player";
 
 function App() {
   return (
     <Main>
       <h1>Scorekeeper Version A</h1>
       <ScoreBox>
+        <Player>Hans</Player>
+        <Player>Wurst</Player>
         <Button>Reset scores</Button>
         <Button>Reset all</Button>
       </ScoreBox>

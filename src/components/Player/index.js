b562a3a9d4aee195e9eb4fb5e7ba9b0ffa7b83d6
Sebastian Scherbes <github@scherbes.de>
2022-09-27 14:46:13 +0200

    change player component

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,7 +1,32 @@
 import styled from "styled-components";
+import Button from "../Button";
 
-const Player = styled.p`
-  align-self: flex-start;
+const Player = ({ name, score }) => {
+  return (
+    <PlayerItem>
+      <PlayerName>{name}</PlayerName>
+      <ScoreArea>
+        <Button text="-" width="1.4rem" height="1.4rem" />
+        <PlayerScore>{score}</PlayerScore>
+        <Button width="1.4rem" height="1.4rem" />
+      </ScoreArea>
+    </PlayerItem>
+  );
+};
+
+const PlayerItem = styled.li`
+  width: 100%;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PlayerName = styled.p``;
+const PlayerScore = styled.p``;
+
+const ScoreArea = styled.span`
+  display: flex;
+  gap: 1em;
 `;
 
 export default Player;
